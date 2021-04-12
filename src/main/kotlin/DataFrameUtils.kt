d034b3f51c62f7d323b63092906c6ca7cb5cdce7
Giovanni <giovanni.ciatto@unibo.it>
2021-04-12 14:24:18 +0200

    + data frame utils

--- src/main/kotlin/DataFrameUtils.kt
+++ src/main/kotlin/DataFrameUtils.kt
@@ -1,12 +1,17 @@
 import smile.data.DataFrame
+import smile.data.Tuple
+import smile.data.type.DataTypes
+import smile.data.type.StructField
+import smile.data.type.StructType
 import smile.feature.Normalizer
 import kotlin.random.Random
+import kotlin.streams.asSequence
 
 fun DataFrame.randomSplit(percent: Double, seed: Int = 0): Pair<DataFrame, DataFrame> {
     val r1 = Random(seed)
     val r2 = Random(seed)
-    val test = DataFrame.of(stream().filter { r1.nextDouble() < percent })
-    val train = DataFrame.of(stream().filter { r2.nextDouble() >= percent })
+    val test = DataFrame.of(this.stream().filter { r1.nextDouble() < percent })
+    val train = DataFrame.of(this.stream().filter { r2.nextDouble() >= percent })
     return train to test
 }
 
@@ -14,3 +19,35 @@ fun DataFrame.normalize(): DataFrame {
     val n = Normalizer(Normalizer.Norm.L2)
     return n.transform(this)
 }
+
+val DataFrame.lastColumnIndex: Int
+    get() = this.ncols() - 1
+
+fun DataFrame.inputs(outputColumn: Int = lastColumnIndex): DataFrame =
+    this.drop(outputColumn)
+
+fun DataFrame.categories(i: Int = lastColumnIndex): Set<Any> =
+    this.stream().map { it[i] }.distinct().asSequence().toSet()
+
+fun DataFrame.classes(outputColumn: Int = lastColumnIndex): DataFrame {
+    val classes = categories(outputColumn)
+    return DataFrame.of(
+        this.select(outputColumn).stream().map {
+            val klass = classes.indexOf(it[0])
+            if (klass < 0) throw IllegalStateException("Invalid class: ${it[0]}")
+            Tuple.of(arrayOf(klass), StructType(StructField("Class", DataTypes.IntegerType)))
+        }
+    )
+}
+
+fun DataFrame.outputs(outputColumn: Int = lastColumnIndex): DataFrame =
+    this.select(outputColumn)
+
+fun DataFrame.inputsArray(outputColumn: Int = lastColumnIndex): Array<DoubleArray> =
+    this.inputs(outputColumn).toArray()
+
+fun DataFrame.outputsArray(outputColumn: Int = lastColumnIndex): DoubleArray =
+    this.apply(outputColumn).toArray()
+
+fun DataFrame.classesArray(outputColumn: Int = lastColumnIndex): IntArray =
+    this.classes(outputColumn).intVector(0).toIntArray()
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,16 @@
-import it.unibo.tuprolog.core.Clause
-import it.unibo.tuprolog.core.Integer
-import it.unibo.tuprolog.core.Struct
-import it.unibo.tuprolog.theory.MutableTheory
-import it.unibo.tuprolog.theory.Theory
 import smile.base.mlp.Layer
 import smile.base.mlp.OutputFunction
 import smile.classification.*
+import smile.io.Read
 import smile.math.TimeFunction
-import smile.math.kernel.GaussianKernel
-import smile.math.kernel.MercerKernel
-import smile.validation.Hyperparameters
 import smile.validation.metric.Accuracy
 
 fun main() {
-    val dataset = Data("iris.data")
-    val x = dataset.xtrain
-    val y = dataset.ytrain.map { it.toInt() }.toIntArray()
+
+    val dataset = Read.csv("datasets/iris.data")
+    val (train, test) = dataset.randomSplit(0.3)
+    val x = train.inputsArray()
+    val y = train.classesArray()
 
     val model: MLP = mlp(
         x, y,
@@ -29,35 +24,32 @@ fun main() {
 
     val models = listOf(model, knn, log, rbf)
 
-    models.forEach { mod ->
-        println(Accuracy.of(dataset.ytest.map { it.toInt() }.toIntArray(), mod.predict(dataset.xtest)))
+    for (mod in models) {
+        println(Accuracy.of(test.classesArray(), mod.predict(test.inputsArray())))
     }
 
-    models.forEach { mod ->
-        dataset.xtest.forEach { print("${mod.predict(it)} ") }
+    for (mod in models) {
+        for (testInput in test.inputsArray()) {
+            print("${mod.predict(testInput)} ")
+        }
         println()
     }
-    dataset.ytest.forEach { print("${it.toInt()} ") }
-    println()
-/*
-    val arti = Data("arti.csv")
-    val model2 = rbfnet(
-        arti.xtrain,
-        arti.ytrain.map { it.toDouble() }.toDoubleArray(),
-        4,
-        true)
-    println(model2.metric(arti.xtest, arti.ytest.map { it.toDouble() }.toDoubleArray()).MAD)
 
-    val model3 = regressor(
-        arti.xtrain,
-        arti.ytrain.map { it.toDouble() }.toDoubleArray(),
-        arrayOf(Layer.tanh(35), Layer.tanh(15), Layer.mse(1, OutputFunction.LINEAR)),
-        200,
-        TimeFunction.linear(0.1, 10000.0, 0.05))
+    test.classesArray().forEach { print("$it ") }
 
-    println(model3.metric(arti.xtest, arti.ytest.map { it.toDouble() }.toDoubleArray()).MAD)
+//    val arti = Data("arti.csv")
+//    val model2 = rbfnet(
+//        arti.xtrain,
+//        arti.ytrain.map { it.toDouble() }.toDoubleArray(),
+//        4,
+//        true)
+//    println(model2.metric(arti.xtest, arti.ytest.map { it.toDouble() }.toDoubleArray()).MAD)
+//
+//    val model3 = regressor(
+//        arti.xtrain,
+//        arti.ytrain.map { it.toDouble() }.toDoubleArray(),
+//        arrayOf(Layer.tanh(35), Layer.tanh(15), Layer.mse(1, OutputFunction.LINEAR)),
+//        200,
+//        TimeFunction.linear(0.1, 10000.0, 0.05))
 
-    val theory = MutableTheory.empty()
-    theory.assertA(Clause.of(Struct.of("f", Integer.of(1))))
-    */
 }
